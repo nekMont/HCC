296c2281088781f7848a8982c8355a0502646c2d
JaysonRusJ <98131260+JaysonRusJ@users.noreply.github.com>
2022-11-16 08:25:38 -0700

    Added sorting and searching functionalities
    reader.java - updated to write appropriate popup.html 
    popup.js - has 'sortByColumn' and 'tableSearch' to enable sorting and searching functionalities. EventListeners for header clicks and search bar typing
    popup.css - added '.table-sortable' code to create little triangle in headers when sorting
    popup.html - Added formal html tags. Added Search bar and new column to have question # and name separate. Now links with popup.css and popup.js

--- LeetCodeRatio/popup.js
+++ LeetCodeRatio/popup.js
@@ -1,2 +1,139 @@
+// i don't know what this does but i (Jason) could not get the
+// search and sorting to work with it. 
+//object.addEventListener("click", redirect);
 
-object.addEventListener("click", redirect);
+document.getElementById("myInput").addEventListener("keyup",tableSearch);
+
+/**
+ * Sorts the HTML table
+ * @param {HTMLTableElement} table to sort 
+ * @param {number} colum the index of the col to sort 
+ * @param {boolean} determines if sorting will be ascending order 
+ * https://www.youtube.com/watch?v=8SL_hM1a0yo reference YT video 
+ */
+function sortByColumn(table, colum, asc = true) {
+
+   const dirModifier = asc ? 1: -1; //check if ascending
+   const tBody = table.tBodies[0];
+   const rows = Array.from(tBody.querySelectorAll("tr")); //selects every table row, gets an array of tr instead of node list 
+
+   //sort each row 
+   const sortedRow = rows.sort((a,b) => {
+
+      // string sorting (question name and ratio)
+      if (colum == 1 || colum == 5) {
+
+         const aColText = a.querySelector(`td:nth-child(${colum + 1})`).textContent.trim();
+         const bColText = b.querySelector(`td:nth-child(${colum + 1})`).textContent.trim();
+
+         if (colum == 5) 
+            return aColText > bColText ? (-1 * dirModifier) : (1 * dirModifier);
+
+         // sort for question name and ratio
+         return aColText > bColText ? (1 * dirModifier) : (-1 * dirModifier);
+
+      }
+
+      //  difficulty sorting
+      else if (colum == 2) {
+
+         const aColText = a.querySelector(`td:nth-child(${colum + 1})`).textContent.trim();
+         const bColText = b.querySelector(`td:nth-child(${colum + 1})`).textContent.trim();
+
+         // same difficulties
+         if (aColText.localeCompare(bColText) == 0)
+            return -1 * dirModifier;
+
+         // a is easy but b is not easy
+         if (aColText.localeCompare("Easy") == 0)
+            return -1 * dirModifier;
+
+         // a is hard
+         if (aColText.localeCompare("Hard") == 0)
+            return 1 * dirModifier;
+
+         // a is medium and b is easy
+         if (aColText.localeCompare("Medium") == 0 && bColText.localeCompare("Easy") == 0)
+            return 1 * dirModifier;
+
+         // a is medium and b is hard
+         else (aColText.localeCompare("Medium") == 0 && bColText.localeCompare("Hard") == 0)
+            return -1 * dirModifier;
+
+      }
+
+      // number sorting
+      else {
+
+         const aColPrice = parseFloat(a.querySelector(`td:nth-child(${colum + 1})`).textContent);
+         const bColPrice = parseFloat(b.querySelector(`td:nth-child(${colum + 1})`).textContent);
+
+         if (colum == 0)
+            // sorting for question #
+            return aColPrice > bColPrice ? (1 * dirModifier) : (-1 * dirModifier);
+         
+         else
+            // sorting for likes, dislikes
+            return aColPrice > bColPrice ? (-1 * dirModifier) : (1 * dirModifier);
+
+      }
+
+         
+   });
+
+   // remove all existing TRs from the table
+   while (tBody.firstChild) {
+      tBody.removeChild(tBody.firstChild);
+   }
+
+   // re-add the newly sorted rows
+   tBody.append(...sortedRow);
+
+   //remember how the col is currently sorted 
+   table.querySelectorAll("th").forEach(th => th.classList.remove("th-sort-asc", "th-sort-desc")); 
+   table.querySelector(`th:nth-child(${colum + 1})`).classList.toggle("th-sort-asc", asc);
+   table.querySelector(`th:nth-child(${colum + 1})`).classList.toggle("th-sort-desc", !asc);
+
+} // close sortbyCloum
+
+
+// whenever a header is click sort its row
+document.querySelectorAll(".table-sortable th").forEach(headerCell => {
+   headerCell.addEventListener("click", () => {
+      const tableElement = headerCell.parentElement.parentElement.parentElement;
+      const headerIndex = Array.prototype.indexOf.call(headerCell.parentElement.children, headerCell);
+      const currentIsAscending = headerCell.classList.contains("th-sort-asc");
+
+      sortByColumn(tableElement, headerIndex, !currentIsAscending);
+   });
+});
+
+
+// Enables search feature to find a specific question by name
+// followed tutorial by british guy: https://www.youtube.com/watch?v=eLQhybnA9hw&t=619s
+function tableSearch() {
+   let input, filter, table, tr, td, txtValue;
+
+   // initialize variables
+   input = document.getElementById("myInput");
+   filter = input.value.toUpperCase();
+   table = document.getElementById("ourTable");
+   tr = table.getElementsByTagName("tr");
+
+   for (let i = 0; i < tr.length; i++) {
+
+      td = tr[i].getElementsByTagName("td")[1];
+
+      if (td) {
+         txtValue = td.textContent || td.innerText;
+         if(txtValue.toUpperCase().indexOf(filter) > -1) {
+            tr[i].style.display = "";
+         }
+         else {
+            tr[i].style.display = "none";
+         }
+      }
+
+   } // close for
+
+} // close tableSearch
--- LeetCodeRatio/reader.java
+++ LeetCodeRatio/reader.java
@@ -3,10 +3,18 @@ import java.util.*;
 
 public class reader {
 
-   public static void main (String args[]) throws IOException {
+   public static void main(String args[]) throws IOException {
+
+      writePopup();
+
+   } // close main
+
+   // Reads from "LeetCodeRatio.csv" to write html to 
+   // "popup.html" and creates a table
+   public static void writePopup () throws IOException {
    
    try {
-	System.out.println("file ran");
+   
       // declare file to be the webscrapped info
    	File f = new File("LeetCodeRatio.csv");
    	// declare scanner to look at data file
@@ -18,20 +26,41 @@ public class reader {
    	
    	// write initial html tags
    	pen.write("<html>\n");
-   	// set title for tab
-   	pen.write("<title>Like to Dislike Ratio!</title>\n");
-   	// create table with black background
-   	pen.write("<table bgcolor=\"black\" width=\"750\">\n");
-   	// create table row
-   	pen.write("\t<tr bgcolor=\"salmon\">\n");
-   	// add colomun headers to table
-   	pen.write("\t\t<th width=\"300\">Question</th>\n");
-   	pen.write("\t\t<th width=\"100\">Difficulty</th>\n");
-   	pen.write("\t\t<th width=\"100\">Likes</th>\n");
-   	pen.write("\t\t<th width=\"100\">Dislikes</th>\n");
-   	pen.write("\t\t\t<th width=\"150\">Like-Dislike Ratio</th>\n");
    	
-   	pen.write("\t</tr>\n");
+   	pen.write("<head>\n");
+   	
+   	// set title for tab
+   	pen.write("\t<title>Like to Dislike Ratio!</title>\n");
+   	
+   	// link html to css file
+   	pen.write("\t<link rel=\"stylesheet\" href=\"./popup.css\">\n");
+   	
+   	// formal html tags
+   	pen.write("</head>\n\n");
+   	pen.write("<body>\n");
+
+      // search bar
+	  pen.write("\t<label for=\"myInput\">Search: </label>\n");
+      pen.write("\t<input type=\"text\" id=\"myInput\" placeholder=\"Question Name\">\n");
+	  pen.write("\t<br><br>\n");
+   	
+   	// create table with black background
+   	pen.write("\t<table class=\"table-sortable\" id=\"ourTable\" bgcolor=\"black\" width=\"900\">\n");
+   	// create table header row
+   	pen.write("\t\t<thead>\n");
+   	pen.write("\t\t\t<tr bgcolor=\"#C9C0BD\">\n");
+   	// add colomun headers to table
+   	pen.write("\t\t\t\t<th width=\"50\">#</th>\n");
+   	pen.write("\t\t\t\t<th width=\"400\">Question</th>\n");
+   	pen.write("\t\t\t\t<th width=\"100\">Difficulty</th>\n");
+   	pen.write("\t\t\t\t<th width=\"100\">Likes</th>\n");
+   	pen.write("\t\t\t\t<th width=\"100\">Dislikes</th>\n");
+   	pen.write("\t\t\t\t<th width=\"150\">Like-Dislike Ratio</th>\n");
+   	
+	// closing tags
+   	pen.write("\t\t\t</tr>\n");
+   	pen.write("\t\t</thead>\n");
+   	pen.write("\t\t<tbody>\n");
    	
    	// while there is stuff to read from .csv
    	while (scan.hasNext()) {
@@ -43,9 +72,16 @@ public class reader {
    	   // count the commas in the read line (for error cases)
    	   int commaCount = countCommas(line);
    	   
+   	   
+   	   if (commaCount > 1)
+   	      line = line.substring(findIntegerIndex(line));
+   	   
+   	   // debug printing
+   	   //System.out.println(line);
+   	   
    	   // declare empty strings 
    	   String question = "", difficulty = "", likes = "", dislikes = "", link = "";
-   	   String likeDislikeRatio = "";
+   	   String likeDislikeRatio = "", number = "";
    	   
    	   // error cases there is a comma in the question name
    	   if (commaCount > 5) {
@@ -56,12 +92,18 @@ public class reader {
    	   // process the information from a line
    	   if (commaCount == 5) {
    	   
+   	      // grab question number
+   	      number = line.substring(0,line.indexOf('.'));
+   	      line = line.substring(line.indexOf(' ') + 1);
+   	   
    	      // grab the question name store in String question
    	      while (count >= 0) {
    	         question = question + line.substring(0,line.indexOf(','));
    	         line = line.substring(line.indexOf(',') + 1);
    	         count--;
    	      } // close while
+   	      
+   	      //System.out.println(number);
 
    	      // grab the acceptance percentage
    	      //acceptance = line.substring(0,line.indexOf(','));
@@ -89,35 +131,45 @@ public class reader {
    	      likeDislikeRatio = likeDislikeRatio.substring(2,4) + "." + likeDislikeRatio.substring(4,6) + "%";
    	   
    	      //create new row in table
-   	      pen.write("\t<tr bgcolor=\"seashell\" align=\"center\">\n");
+   	      pen.write("\t\t\t<tr bgcolor=\"seashell\" align=\"center\">\n");
+   	      
+   	      // add question number cell
+   	      pen.write("\t\t\t\t<td>" + number + "</td>\n");
    	   
    	      // add question cell
-   	      pen.write("\t\t<td align=\"left\"><a href=\"" + link + "\" target='_blank' rel='noopener noreferrwe'>" + question + "</a></td>\n");
+   	      pen.write("\t\t\t\t<td align=\"left\"><a href=\"" + link + "\" target='_blank' rel='noopener noreferrwe'>" + question + "</a></td>\n");
    	   
    	      // add difficulty cell with color!
    	      if (difficulty.contains("Easy"))
-   	         pen.write("\t\t<td style=\"color:MediumSeaGreen;\">" + difficulty + "</td>\n");
+   	         pen.write("\t\t\t\t<td style=\"color:MediumSeaGreen;\">" + difficulty + "</td>\n");
    	      else if (difficulty.contains("Medium"))
-   	   	   pen.write("\t\t<td style=\"color:Orange;\">" + difficulty + "</td>\n");
+   	   	   pen.write("\t\t\t\t<td style=\"color:Orange;\">" + difficulty + "</td>\n");
    	      else 
-   	   	   pen.write("\t\t<td style=\"color:Tomato;\">" + difficulty + "</td>\n");
+   	   	   pen.write("\t\t\t\t<td style=\"color:Tomato;\">" + difficulty + "</td>\n");
    	   
    	      // add like cell
-   	      pen.write("\t\t<td>" + likes + "</td>\n");
+   	      pen.write("\t\t\t\t<td>" + likes + "</td>\n");
    	      // add dislike cell
-   	      pen.write("\t\t<td>" + dislikes + "</td>\n");
+   	      pen.write("\t\t\t\t<td>" + dislikes + "</td>\n");
    	      // add like-dislike ratio cell
-   	      pen.write("\t\t\t<td>" + likeDislikeRatio + "</td>\n");
+   	      pen.write("\t\t\t\t<td>" + likeDislikeRatio + "</td>\n");
    	   
    	      //  close table
-   	      pen.write("\t</tr>\n");
+   	      pen.write("\t\t\t</tr>\n");
    	   
    	   } // close if
    	
    	} // close while
    	
    	// ending tags
-   	pen.write("</table>\n");
+   	pen.write("\t\t</tbody>\n");
+   	pen.write("\t</table>\n");
+   	
+   	// call javascript
+   	pen.write("\t<script src=\"./popup.js\"></script>\n");
+   	
+	// closing tags
+   	pen.write("</body>\n");
    	pen.write("</html>");
    	
    	// close the readers
@@ -134,7 +186,7 @@ public class reader {
    
    }
    
-   } // close main
+   } // close writePopup
    
    // counts commas in a given string
    public static int countCommas (String line) {
@@ -158,5 +210,21 @@ public class reader {
       return count;
    
    } // close countCommas
+   
+   // finds the first integer in a string
+   public static int findIntegerIndex (String line) {
+   
+      int count = 0;
+   
+   	while (true) {
+   	
+   	   if (line.charAt(count) >= '0' && line.charAt(count) <= '9')
+   	      return count;
+   	      
+   	   count++;
+   	
+   	} // close while
+   
+   } // close find/integerIndex
 
 } // close class
